a1d843b05c259615a1f224cc1c33891ca8af226a
Eugene Chia <eugeneckk@yahoo.com>
2024-12-30 10:39:17 +0800

    added connection output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "dev_ip" {
-    value = aws_instance.dev_node.public_ip
+  value = aws_instance.dev_node.public_ip
+}
+
+output "connection_script" {
+
+  value = templatefile("${var.host_os}-ssh-config.tpl", {
+    hostname = aws_instance.dev_node.public_ip,
+    user     = "ubuntu",
+  identityfile = "~/.ssh/tf-spacelift" })
+
 }
