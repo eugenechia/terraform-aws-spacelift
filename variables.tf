1b70ab46f95497f69ee08750c5e2069da7c007c7
Eugene Chia <eugeneckk@yahoo.com>
2025-01-01 20:44:13 +0800

    Updated Variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 
 variable "host_os" {
-  type    = string
-  default = "linux"
+  type = string
+  //default = "linux"
 }
 
 variable "aws_region" {
   type = string
-  default = "ap-southeast-1"
+  //default = "ap-southeast-1"
 }
