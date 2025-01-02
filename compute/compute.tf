f99e975e538d05b37e0a843a87160c79d83963f6
Eugene Chia <eugeneckk@yahoo.com>
2025-01-02 10:21:05 +0800

    Updated compute.tf

--- compute/compute.tf
+++ compute/compute.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "spacelift_auth" {
 resource "aws_instance" "dev_node" {
   instance_type          = var.instance_type
   ami                    = data.aws_ami.server_ami.id
-  key_name               = var.key_name
+  key_name               = aws_key_pair.spacelift_auth.id
   vpc_security_group_ids = var.security_group_id
   subnet_id              = var.subnet_id
   user_data              = file("${path.module}/userdata.tpl")
